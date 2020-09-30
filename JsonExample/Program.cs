4c6b862285d35c458bfd7343d854d8dbed201163
DariaCagle <dani.merchard@gmail.com>
2020-09-30 16:18:41 +0300

    serializer

---
+++ JsonExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace JsonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User { Login = "SomeLogin", PassWord = "SomeComplicatedPassword" };
+            string json = JsonConvert.SerializeObject(user, Formatting.Indented);
+            Console.WriteLine(json);
+            User restoredUser = JsonConvert.DeserializeObject<User>(json);
+        }
+    }
+}
